f1b6ee8217728ea4048d17fdd4f1d54e231408d6
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-09-18 16:43:22 +0530

    [Rupali]Feat:insert data into table

--- addressbook_services.sql
+++ addressbook_services.sql
@@ -11,5 +11,17 @@ CREATE TABLE address(
 	PhoneNumber VARCHAR(12) NOT NULL,
 	Email VARCHAR(100) NOT NULL,
 );
+--uc-3
+-- To go to the database created
+use addressbook_services;
 
-
+-- To insert into the table created
+INSERT INTO address(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email) 
+VALUES
+('Rupali','Deshmukh','2nd cros Ravi Nagar','Banglore','Karnataka','58082','8123351458','rupali@gmail.com'),
+('Nikita ','Kumar','3nd cros Jaya Nagar','Banglore','Gujarat','587682','934747358','nikita@gmail.com'),
+('Asha','Kumari','4nd cros Indira Nagar','Banglore','MH','80534','1234567896','asha@gmail.com'),
+('Porvi','Shetty','2nd cros Rajaji Nagar','Banglore','Andra','80453','0987654321','porvi@gmail.com'),
+('Raja','M','2nd cros Rajaji Nagar','Gadag','Andra','80453','0987654321','raja@gmail.com');
+ 
+ select * from address;
