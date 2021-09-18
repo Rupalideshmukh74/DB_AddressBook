c2c7abedccb18a439d42776c206c722f8d36dadb
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-09-18 16:09:56 +0530

    [Rupali]Feat:Table Created

--- addressbook_services.sql
+++ addressbook_services.sql
@@ -1,5 +1,15 @@
 CREATE DATABASE addressbook_services;
 use addressbook_services;
-
+CREATE TABLE address(
+	BookID INT IDENTITY(1, 1) PRIMARY KEY,
+	FirstName VARCHAR(255)  NOT NULL,
+	LastName VARCHAR(255)  NOT NULL,
+	Address VARCHAR(250) NOT NULL,
+	City VARCHAR(100) NOT NULL,
+	State VARCHAR(100) NOT NULL,
+	Zip VARCHAR(6) NOT NULL,
+	PhoneNumber VARCHAR(12) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+);
 
 
